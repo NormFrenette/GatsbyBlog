843a13cfb34ab6589170b60003851b170fef71fc
Normand Frenette <eppfren@gmail.com>
2021-12-18 11:39:00 -0500

    used functions to define jsx element Manu and Listings - it works

--- src/pages/blog-index.js
+++ src/pages/blog-index.js
@@ -8,70 +8,58 @@ import Seo from "../components/seo"
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark
-  console.log("+++++++++++++",posts.group)
 
-  const test = (arr) => {
-      arr.map(item  => {
-        if (item.fieldValue != null) {
-            return(
-                <li key={item.fieldValue}>
-                    {item.fieldValue}
-                    <ol style={{ listStyle: `none` }}>
-                        if (item.group != null) {
-                            test(item.group)
-                        }
-                        else if (item.nodes != null){
-                            test(item.nodes)
-                        }
-                        
-                    </ol>
-                </li>
-            )
-        }
-        else {
-            const title = item.frontmatter.title || item.fields.slug
-            return(
-                <li key={item.fields.slug}>
-                    <article
-                    className="post-list-item"
-                    itemScope
-                    itemType="http://schema.org/Article"
-                    >
-                        <header>
-                            <h2>
-                            <Link to={item.fields.slug} itemProp="url">
-                                <span itemProp="headline">{title}</span>
-                            </Link>
-                            </h2>
-                            <small>{item.frontmatter.date}</small>
-                        </header>
-                        <section>
-                            <p
-                            dangerouslySetInnerHTML={{
-                                __html: item.frontmatter.description,
-                            }}
-                            itemProp="description"
-                            />
-                        </section>
-                    </article>
-                </li>
-            )
-        }
-    })
-  }
+function Listing ( {item} ) {
+    return(
+        <li key={item.fields.slug}>
+                <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+                >
+                    <header>
+                        <h2>
+                        <Link to={item.fields.slug} itemProp="url">
+                            <span itemProp="headline">{item.frontmatter.title}</span>
+                        </Link>
+                        </h2>
+                        <small>{item.frontmatter.date}</small>
+                    </header>
+                    <section>
+                        <p
+                        dangerouslySetInnerHTML={{
+                            __html: item.frontmatter.description,
+                        }}
+                        itemProp="description"
+                        />
+                    </section>
+                </article>
+            </li>
+    )
+}
+
+function Menu( {a} ) {
+    return(
+        <ul>
+        {a.map((post) => (
+            post.group ?<li key={post.fieldValue}>{post.fieldValue}
+            {<Menu a={post.group}/>}</li> :
+                post.nodes ?<li key={post.fieldValue}>{post.fieldValue}
+                {<Menu a={post.nodes}/>}</li> :
+                <Listing item={post} />
+        ))}
+        </ul>
+    );
+}
+
+
   const theGroup = posts.group
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="All posts" />
-
-      <ol style={{ listStyle: `none` }}>
-        {theGroup.map(post => {
-            if (post.group != null) {
-            return(<li key={post.fieldValue}>{post.fieldValue}</li>)
-            }
-        })}
+        
+      <Menu a={theGroup}/>
       
-      </ol> 
     </Layout>
   )
 }
