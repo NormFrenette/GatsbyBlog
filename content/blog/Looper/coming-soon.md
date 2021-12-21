42456f7b11d5f7fdda1ddbe3c1706065f84537b0
Normand Frenette <eppfren@gmail.com>
2021-12-21 10:42:43 -0500

    moved blog-index to main index page - backup previous to old-index

---
+++ content/blog/Looper/coming-soon.md
@@ -0,0 +1,8 @@
+---
+title: Coming Soon - Rapsberry Pi Looper
+date: "2021-12-21T10:00:00.000Z"
+description: Here will be multiple blog posts on the Raspery Pi Looper and accompanying ios app.
+mainTag: general
+---
+
+Here will be multiple blog posts on the Raspery Pi Looper and accompanying ios app.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { useCallback, useState } from "react"
 import { Link, graphql } from "gatsby"
 
 //import Bio from "../components/bio"
@@ -7,61 +8,94 @@ import Seo from "../components/seo"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+  const posts = data.allMarkdownRemark
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <Seo title="All posts" />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="All posts" />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
+function Listing ( {item} ) {
+    return(
+        <li key={item.fields.slug}>
+                <article
                 className="post-list-item"
                 itemScope
                 itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
+                >
+                    <header>
+                        <h5 style={{marginTop: 21, marginBottom: 12, color: '#005b99'}}>
+                        <Link to={item.fields.slug} itemProp="url">
+                            <span itemProp="headline">{item.frontmatter.title}</span>
+                        </Link>
+                        </h5>
+                        <small>{item.frontmatter.date}</small>
+                    </header>
+                    <section>
+                        <p
+                        dangerouslySetInnerHTML={{
+                            __html: item.frontmatter.description,
+                        }}
+                        itemProp="description"
+                        />
+                    </section>
+                </article>
             </li>
-          )
-        })}
-      </ol>
+    )
+}
+
+function LiToggle ( {id,inserted}) {
+    const [state, setState] = useState(false);
+    
+    return( 
+        <li key={id} style={{marginBottom: 21, fontSize: '1.25rem'}}>
+        <button style={{padding: '0', border: '0'}} onClick={() => setState(!state)}>{state ? '-' : '+'}</button> {id}
+        {<Menu a={inserted} isTop={state}/>}
+        </li>
+    )
+
+}
+/*
+{a.map((post) => (
+            post.group ?<li key={post.fieldValue}>{post.fieldValue} 
+            {<Menu a={post.group} isTop={false}/>}</li> :
+                post.nodes ?<li key={post.fieldValue}>{post.fieldValue} 
+                {<Menu a={post.nodes} isTop={false}/>}</li> :
+                <Listing item={post} />
+        ))}
+        */
+
+function Menu( {a,isTop=true} ) {
+
+    return(
+        <ul style={{listStyleType: "none"}} className={`${isTop ? 'is-visible': 'is-not-visible' }`}>
+        {a.map((post) => (
+            post.group ?<LiToggle id={post.fieldValue} inserted={post.group} /> :
+                post.nodes ?<LiToggle id={post.fieldValue} inserted={post.nodes} />:
+                <Listing item={post} />
+        ))}
+        </ul>
+    );
+}
+
+
+  const theGroup = posts.group
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Seo title="All posts" />
+        
+      <Menu a={theGroup}/>
+      
     </Layout>
   )
 }
 
 export default BlogIndex
+
+/*
+<p>{posts.group[0].fieldValue} ,  {posts.group[0].group[0].fieldValue} , 
+      {posts.group[0].group[0].nodes[0].fields.slug} , 
+      {posts.group[0].group[0].nodes[0].id} , 
+      {posts.group[0].group[0].nodes[0].frontmatter.title} , 
+      {posts.group[0].group[0].nodes[0].frontmatter.date} , 
+      {posts.group[0].group[0].nodes[0].frontmatter.description} , 
+      </p>
+*/
 
 export const pageQuery = graphql`
   query {
@@ -70,18 +104,24 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      nodes {
-        excerpt
-        fields {
-          slug
+    allMarkdownRemark {
+        group(field: fields___folder) {
+        fieldValue
+        group(field: frontmatter___mainTag) {
+            fieldValue
+            nodes {
+            fields {
+                slug
+            }
+            id
+            frontmatter {
+                title
+                description
+                date(formatString: "MMMM DD, YYYY")
+            }
+            }
         }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
         }
-      }
     }
   }
 `
