84499e26d559dbb3ae71b82b053469a2d667bb5d
Normand Frenette <eppfren@gmail.com>
2023-09-26 06:48:47 -0400

    testing simple function

--- src/functions/form.js
+++ src/functions/form.js
@@ -1,45 +1,6 @@
-const sendgrid = require("@sendgrid/mail")
-//Your API Key from Sendgrid
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
-const message = {
-  //Your authorized email from SendGrid
-  from: process.env.SENDGRID_AUTHORIZED_EMAIL,
-  to: process.env.SENDGRID_TO_EMAIL,
-  cc: process.env.SENDGRID_CC_EMAIL,
-}
-
-exports.handler =  async (event, context, callback) => {
-    const data = JSON.parse(event.body)
-    const test = data["test"].trim().toLowerCase()
-    const msgToSend = `Message fom Contact Form:
-    from: ${data["name"]}
-    email: ${data["email"]}
-    message: ${data["message"]}
-`   
-
-    if (test == "lion") {
-        message.subject="Message from Blog"
-        message.text = msgToSend
-
-        try{
-            await sendgrid.send(message)
-        
-            return {
-              statusCode: 200,
-              body: "OK"
-            }
-          } catch(e){
-            return {
-              statusCode: e.code,
-              body: e.message
-            }
-          }
-    }
-    else {
-        return {
-            statusCode: 200,
-            body: "invalid"
-          }
-    }
-  }
-
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: "ok",
+  };
+};
