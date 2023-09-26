94303e72e0c329cee8b2c20f9c4c30585f422354
Normand Frenette <eppfren@gmail.com>
2023-09-26 09:32:22 -0400

    everything inside try

--- src/api/form.js
+++ src/api/form.js
@@ -1,15 +1,16 @@
 const sendgrid = require("@sendgrid/mail")
 //Your API Key from Sendgrid
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
-const message = {
-  //Your authorized email from SendGrid
-  from: process.env.SENDGRID_AUTHORIZED_EMAIL,
-  to: process.env.SENDGRID_TO_EMAIL,
-  cc: process.env.SENDGRID_CC_EMAIL,
-}
+
 
 const contactFormHandler = (req, res) => {
     try {
+        sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
+        const message = {
+        //Your authorized email from SendGrid
+        from: process.env.SENDGRID_AUTHORIZED_EMAIL,
+        to: process.env.SENDGRID_TO_EMAIL,
+        cc: process.env.SENDGRID_CC_EMAIL,
+        }
         const test = req.body["test"].trim().toLowerCase()
         const msgToSend = `Message fom Contact Form:
 from: ${req.body["name"]}
